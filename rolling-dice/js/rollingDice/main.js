713a9ca264ba4cb53a90993f90e1a5b8f55a7fbb
LukeSlater02 <lukeslater101@gmail.com>
2022-05-20 11:44:12 -0500

    done

--- rolling-dice/js/rollingDice/main.js
+++ rolling-dice/js/rollingDice/main.js
@@ -1,1 +1,43 @@
 // Put your code here
+console.log("Let's roll some dice, baby!");
+console.log("---------------------------");
+
+const Roll = () => {
+    let result = {
+        value: Math.floor(Math.random() * 6) + 1,
+        stringValue: ""
+    }
+    switch (result.value) {
+        case 1:
+            result.stringValue = "one";
+            break;
+        case 2:
+            result.stringValue = "two";
+            break;
+        case 3:
+            result.stringValue = "three";
+            break;
+        case 4:
+            result.stringValue = "four";
+            break;
+        case 5:
+            result.stringValue = "five";
+            break;
+        case 6:
+            result.stringValue = "six";
+            break;
+    }
+    return result
+}
+
+for (let i = 0; i < 10; i++) {
+    let die1 = Roll();
+    let die2 = Roll();
+
+    let message = `${die1.stringValue} + ${die2.stringValue} == ${die1.value + die2.value}`
+    if (die1.value == die2.value) {
+        message += " DOUBLES!";
+    }
+
+    console.log(message);
+}
