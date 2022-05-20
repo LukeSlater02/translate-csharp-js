84aa42e85184785472eb2c3b98dfd5a5dfc763ea
LukeSlater02 <lukeslater101@gmail.com>
2022-05-20 11:13:27 -0500

    done

--- place-names/js/placeNames/main.js
+++ place-names/js/placeNames/main.js
@@ -10,3 +10,14 @@ console.log("All Pace Names");
 for (const name of names) {
   console.log(name);
 }
+
+console.log()
+
+let theNames = names.filter(ele => {
+  if (ele.startsWith("The")){
+    return ele
+  }
+})
+
+console.log("'The' Place Names");
+theNames.forEach(ele => console.log(ele))
